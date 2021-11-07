2da7004db08d49728c75a6b0601647bcae144f03
VentisDev <tommylindev@gmail.com>
2021-11-07 10:19:06 -0800

    commit

--- src/main/java/com/example/demo/student/StudentService.java
+++ src/main/java/com/example/demo/student/StudentService.java
@@ -14,7 +14,6 @@ public class StudentService {
     }
 
     public List<Student> getStudents(){
-
         return studentRepository.findAll();
     }
 
@@ -28,4 +27,3 @@ public class StudentService {
         System.out.println(student);
     }
 }
-n
